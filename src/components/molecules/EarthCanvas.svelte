282dc48f6a5a2186d17b20d0d357fb60cbfbda7e
Yanis B <64079721+Miisterfox@users.noreply.github.com>
2023-01-27 13:12:42 +0100

    saving

--- src/components/molecules/EarthCanvas.svelte
+++ src/components/molecules/EarthCanvas.svelte
@@ -3,9 +3,6 @@
     import * as THREE from "three";
     import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
     import gsap from "gsap";
-    import { ScrollTrigger } from "gsap/ScrollTrigger"
-
-    gsap.registerPlugin(ScrollTrigger);
 
     //Global scene
     const scene = new THREE.Scene();
@@ -24,71 +21,67 @@
     scene.add(new THREE.AmbientLight(0xffffff));
 
     const textureLoader = new THREE.TextureLoader();
-
-    const geometry = new THREE.SphereGeometry(30, 50, 50);
-    const material = new THREE.MeshPhongMaterial({
-        map: textureLoader.load("./src/assets/earthmap1k.jpg"),
-        bumpMap: textureLoader.load("./src/assets/earthbump1k.jpg"),
-        bumpScale: 100,
-    });
-
-    const earth = new THREE.Mesh(geometry, material);
+    const RADIUS = 30;
+    const earth = new THREE.Mesh(
+        new THREE.SphereGeometry(RADIUS, 50, 50, 20),
+        new THREE.MeshPhongMaterial({
+            map: textureLoader.load("./src/assets/earthmap1k.jpg"),
+            bumpMap: textureLoader.load("./src/assets/earthbump1k.jpg"),
+            bumpScale: 100,
+            wireframe: true,
+        })
+    );
     scene.add(earth);
 
+    const spherical = new THREE.Spherical();
+    const nantes = {
+        lon: THREE.Math.degToRad(2.349014),
+        lat: THREE.Math.degToRad(90 - 48.864716),
+    };
+
+    const nantesVector = new THREE.Vector3().setFromSphericalCoords(
+        RADIUS,
+        nantes.lat,
+        nantes.lon
+    );
+    var lineGeom = new THREE.BufferGeometry().setFromPoints([
+        new THREE.Vector3(),
+        nantesVector,
+    ]);
+    var line = new THREE.Line(
+        lineGeom,
+        new THREE.LineBasicMaterial({ color: "yellow" })
+    );
+    earth.add(line);
 
     onMount(() => {
         const renderer = new THREE.WebGLRenderer({
             canvas: document.querySelector("#canvas"),
             antialias: true,
-            alpha:true,
         });
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.update();
 
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
-
         renderer.render(scene, camera);
         function animate() {
             requestAnimationFrame(animate);
             renderer.render(scene, camera);
-
+            controls.update();
             renderer.setSize(window.innerWidth, window.innerHeight);
             renderer.setPixelRatio(window.devicePixelRatio);
         }
 
         animate();
     });
-
-    gsap.to(earth.rotation, {
-        scrollTrigger: {
-            trigger: "#canvas",
-            start: "25% center",
-            end: "center 100px",
-            markers:true,
-            scrub: 1,
-            toggleActions: "restart pause reverse pause",
-        },
-        y : 5,
-    })
-
-    gsap.to(earth.position, {
-        scrollTrigger: {
-            trigger: "#canvas",
-            start: "25% center",
-            end: "center 100px",
-            markers:true,
-            scrub: 1,
-            toggleActions: "restart pause reverse pause",
-        },
-        y : 50,
-    })
-
 </script>
 
 <style>
     canvas {
-        position:relative;
-        top:0;
-        left:0;
+        position: relative;
+        top: 0;
+        left: 0;
     }
 </style>
 
